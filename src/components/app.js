ad89dee50b5b044dc6ffd16dd663af8101082760
jtabach <jeff.n.tabachnick@gmail.com>
2016-05-12 12:04:43 -0700

    initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import { Component } from 'react';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+      <Helmet title="Helmet Test" />
+        Helmet Testing
+      </div>
     );
   }
 }
